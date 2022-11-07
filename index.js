1b4d1be5bdb0ca3a1d948d5757d73ab99f03f665
Your Name Main <you@example.com>
2022-11-07 10:44:54 -0800

    multiple services data load

--- index.js
+++ index.js
@@ -16,10 +16,19 @@ const uri = process.env.MONGODB_USER_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-  const userCollection =client.db('assignmrntProject').collection('user')
+  const userCollection =client.db('assignmrntProject').collection('users')
+
+  // get database services data 
+app.get('/services',async (req,res)=>{
+  const query={}
+  const cursor = userCollection.find(query)
+  const services=await cursor.toArray()
+  res.send(services)
+})
+
+
 
 }
-
 run().catch(err => console.log(err))
 
 app.get('/',(req,res)=>{
